d2fb854c3e45eb237c803d694fdf5b752fb14a75
samharshe <samharshe29@gmail.com>
2024-06-21 19:29:24 -0400

    i remain a shocking developer and am committing a ragtag group of changes mid-modification because i am afraid i will undo it all somehow

--- model/scripts/gaussian_rbf.py
+++ model/scripts/gaussian_rbf.py
@@ -1,7 +1,10 @@
 import torch
 from torch import Tensor
-import numpy as np
 
 def gaussian_rbf(x: Tensor) -> Tensor:
-    cs = torch.tensor(np.arange(0,1.6,0.2))
-    return torch.exp(torch.square((x - cs)) / -.005).float()
+    sigma, c_min, c_max, c_inc, num_c = 0.05, 0, 1.6, 8
+    c_inc = c_max / num_c
+    
+    cs = torch.arange(c_min, c_max, c_inc)
+    
+    return torch.exp(-torch.square((x - cs)) / (2*sigma**2)).float()
